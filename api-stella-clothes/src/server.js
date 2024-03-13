f005e877f9c5f6b1c37da159022f199b91816202
Zé Castro <a20351@alunos.ipca.pt>
2024-03-13 12:17:30 +0000

    firebase, mongoDB and basic config

--- api-stella-clothes/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-const port = process.env.PORT || 3000;
-const host = '0.0.0.0';
-
-app.listen(port, host, () => console.log(`Server is Running! ${port}, Hosted in ${host}`));
