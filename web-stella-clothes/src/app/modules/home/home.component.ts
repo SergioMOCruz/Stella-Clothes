b3da538522efc066192888cb3fc4dfdc6b372f9a
Sérgio Cruz <75474370+SergioMOCruz@users.noreply.github.com>
2024-03-24 16:09:32 +0000

    Add home page strutcture

---
+++ web-stella-clothes/src/app/modules/home/home.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
