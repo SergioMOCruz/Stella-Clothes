adbf84d1f96535d27b5858c671ba1eb4a541c576
Voliextreme <a20366@alunos.ipca.pt>
2024-04-01 23:40:51 +0100

    Login e Registo front end corrigido

--- web-stella-clothes/src/app/app.routes.ts
+++ web-stella-clothes/src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { FourOFourComponent } from './views/four-o-four/four-o-four.component';
-import { LoginComponent } from './views/login/login.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'Home', component: HomeComponent },
   { path: '404', component: FourOFourComponent },
-  { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: '/404' },
 ];
 
