f752e2a6364e050edaecd4c76d4aeba38327eddb
João Antunes <jmbantunes2@gmail.com>
2024-04-02 23:30:58 +0100

    Add my-account view

--- web-stella-clothes/src/app/app.routes.ts
+++ web-stella-clothes/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { FourOFourComponent } from './views/four-o-four/four-o-four.component';
 import { LoginComponent } from './views/login/login.component';
+import { ProfileNavbarComponent } from './views/profile-navbar/profile-navbar.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'Home', component: HomeComponent },
   { path: '404', component: FourOFourComponent },
   { path: 'login', component: LoginComponent},
+  { path: 'profile-navbar', component: ProfileNavbarComponent},
   { path: '**', redirectTo: '/404' },
 ];
 
---
+++ web-stella-clothes/src/app/views/profile-navbar/profile-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-navbar.component.html',
+  styleUrl: './profile-navbar.component.scss'
+})
+export class ProfileNavbarComponent {
+
+}
