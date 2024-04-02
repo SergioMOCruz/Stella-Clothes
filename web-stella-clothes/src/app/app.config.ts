dfa888d99e39b41aaacba33ca89a9380b2ca9952
Voliextreme <a20366@alunos.ipca.pt>
2024-04-02 14:33:58 +0100

    Login Authorization Done

--- web-stella-clothes/src/app/app.config.ts
+++ web-stella-clothes/src/app/app.config.ts
@@ -1,8 +1,47 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  importProvidersFrom as importProvidersFrom_alias,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {
+  provideFirebaseApp,
+  initializeApp as initializeApp_alias,
+} from '@angular/fire/app';
+import { provideAuth, getAuth as getAuth_alias } from '@angular/fire/auth';
+import { provideFirestore } from '@angular/fire/firestore';
+import { provideStorage } from '@angular/fire/storage';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage()),
+    ]),
+    importProvidersFrom_alias(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'loja-online-979c4',
+          appId: '1:640772130099:web:d85b5cf857b87109fa6a46',
+          storageBucket: 'loja-online-979c4.appspot.com',
+          apiKey: 'AIzaSyDQUd8PM3D45W54WDGnjzNl-bKkS-mGJLo',
+          authDomain: 'loja-online-979c4.firebaseapp.com',
+          messagingSenderId: '640772130099',
+        })
+      ),
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+    ),
+    importProvidersFrom_alias(provideAuth(() => getAuth())),
+  ],
 };
--- web-stella-clothes/src/app/views/login/login.component.ts
+++ web-stella-clothes/src/app/views/login/login.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig }
+],
 })
-export class LoginComponent {
+  export class LoginComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+  ngOnInit() {}
+
+  login(email:string, password:string) {
+   const isLogged = this.authService.SignIn(email, password);
+  }
 
 }
+
