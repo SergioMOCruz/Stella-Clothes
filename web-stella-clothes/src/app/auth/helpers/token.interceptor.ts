fc0615ef235ed09d3023c646b420ced741e3ee2a
Sérgio Cruz <75474370+SergioMOCruz@users.noreply.github.com>
2024-04-15 22:39:28 +0100

    Fix cart functionalities and login issues

--- web-stella-clothes/src/app/auth/helpers/token.interceptor.ts
+++ web-stella-clothes/src/app/auth/helpers/token.interceptor.ts
@@ -2,9 +2,7 @@ import { HttpInterceptorFn } from "@angular/common/http";
 import { UserSessionHandlerService } from "../services/helpers/user-session-handler.service";
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-  const _userSession: UserSessionHandlerService = new UserSessionHandlerService();
-
-  const authToken = _userSession.getLocalToken();
+  const authToken = localStorage.getItem('token');
 
   const headers = req.headers.set('Authorization', `Bearer ${authToken}`);
 
--- web-stella-clothes/src/app/modules/cart/cart.component.ts
+++ web-stella-clothes/src/app/modules/cart/cart.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FooterComponent } from "../../layout/shared/footer/footer.component";
 import { NavbarComponent } from "../../layout/shared/navbar/navbar.component";
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Product } from '../../shared/interfaces/products/products';
+import { UserSessionHandlerService } from '../../auth/services/helpers/user-session-handler.service';
 
 @Component({
   selector: 'app-cart',
@@ -14,43 +16,32 @@ import { FormsModule } from '@angular/forms';
 })
 export class CartComponent {
 
-  cartItems: any[] = [
-    {
-      name: "Vestido Floral",
-      size: "M",
-      price: 39.99,
-      quantity: 1,
-      image: "../../../assets/images/vestido.jpg"
-    },
-    {
-      name: "Camiseta Branca",
-      size: "L",
-      price: 14.99,
-      quantity: 2,
-      image: "../../../assets/images/camiseta.jpg"
-    },
-    // Add more items as needed
-  ];
+  isLoggedIn: boolean = true;
+  cartItems: Product[] = [];
+
+  constructor(
+    private _userSession: UserSessionHandlerService,
+    public router: Router,
+  ) {
+    this.isLoggedIn = this._userSession.isLoggedIn();
+  }
 
   removeItem(item: any): void {
-    // Implement the logic to remove the item from the cart
-    // For example:
     const index = this.cartItems.indexOf(item);
     if (index !== -1) {
       this.cartItems.splice(index, 1);
     }
   }
 
-  updateItem(item: any): void {
-    // Implement the logic to update the item in the cart
-  }
-
-  getSubtotal(): number {
+  getSubtotal() {
     let total = 0;
     for (const item of this.cartItems) {
       total += item.price * item.quantity;
     }
-    return total;
+    return total.toFixed(2);
   }
 
+  calculateItemSubtotal(price, qty) {
+    return (price * qty).toFixed(2);
+  }
 }
