6e2ad64ce315b3efb8c39e9c4268b2e1547e778f
Voliextreme <a20366@alunos.ipca.pt>
2024-04-17 22:39:22 +0100

    My Orders Done

--- web-stella-clothes/src/app/app.routes.ts
+++ web-stella-clothes/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './modules/home/home.component';
 import { FourOFourComponent } from './views/four-o-four/four-o-four.component';
 import { AuthGuard } from './auth/guards/auth.guard';
 import { CartComponent } from './modules/cart/cart.component';
+import { MyOrdersComponent } from './modules/my-orders/my-orders.component';
+
 
 export const routes: Routes = [
   // In routes needing of Authentication add { ..., canActivate: [AuthGuard] }
@@ -11,6 +13,8 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '404', component: FourOFourComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'myorders', component: MyOrdersComponent },
+
   { path: '**', redirectTo: '/404' },
 ];
 
---
+++ web-stella-clothes/src/app/modules/my-orders/my-orders.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from "../../layout/shared/footer/footer.component";
+import { NavbarComponent } from "../../layout/shared/navbar/navbar.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+
+interface MyOrder {
+  status: string;
+  date: string;
+  price: string;
+  images: string[];
+}
+
+@Component({
+  selector: 'app-myorders',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, FooterComponent, FormsModule],
+  templateUrl: './my-orders.component.html',
+  styleUrl: './my-orders.component.scss'
+})
+
+export class MyOrdersComponent implements AfterViewInit {
+  orders: MyOrder[] = [
+    
+  ];
+
+  @ViewChild('orderProducts') orderProducts: ElementRef;
+
+  ngAfterViewInit() {
+    this.checkShadow();
+  }
+
+  checkShadow() {
+    const orderWidth = this.orderProducts.nativeElement.clientWidth;
+    const imageWidth = 250;
+    const totalImagesWidth = this.orders.reduce((acc, order) => acc + order.images.length * imageWidth, 0);
+    
+    if (totalImagesWidth > orderWidth) {
+      this.showShadow();
+    } else {
+      this.hideShadow();
+    }
+  }
+
+  showShadow() {
+    const shadowElements = this.orderProducts.nativeElement.getElementsByClassName("order-products-shadow");
+    for (let i = 0; i < shadowElements.length; i++) {
+      shadowElements[i].style.display = "block";
+    }
+  }
+
+  hideShadow() {
+    const shadowElements = this.orderProducts.nativeElement.getElementsByClassName("order-products-shadow");
+    for (let i = 0; i < shadowElements.length; i++) {
+      shadowElements[i].style.display = "none";
+    }
+  }
+}
