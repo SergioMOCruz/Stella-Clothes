d77e204e5c83a4d8c7ec677784a262d011d61e50
João Antunes <jmbantunes2@gmail.com>
2024-04-18 00:25:54 +0100

    Change personal data

--- web-stella-clothes/src/app/app.routes.ts
+++ web-stella-clothes/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './modules/home/home.component';
 import { FourOFourComponent } from './views/four-o-four/four-o-four.component';
 import { LoginComponent } from './views/login/login.component';
 import { ProfileNavbarComponent } from './views/profile-navbar/profile-navbar.component';
+import { ChangeDataComponent } from './views/change-data/change-data.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: '404', component: FourOFourComponent },
   { path: 'login', component: LoginComponent},
   { path: 'profile-navbar', component: ProfileNavbarComponent},
+  { path: 'change-data', component: ChangeDataComponent},
   { path: '**', redirectTo: '/404' },
 ];
 
