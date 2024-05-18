4db9f6668b7c7d003f11f5bf24a37a019bd6dbc0
Zé Castro <a20351@alunos.ipca.pt>
2024-05-19 00:26:28 +0100

    updated calls text

--- api-stella-clothes/controllers/category.js
+++ api-stella-clothes/controllers/category.js
@@ -11,38 +11,15 @@ const getAll = async (req, res) => {
   }
 };
 
-// Create a new category
-const create = async (req, res) => {
-  try {
-    const { description } = req.body;
-
-    // check if category already exists
-    const categoryExists = await Category.findOne({ description });
-    if (categoryExists) {
-      return res.status(400).json({ message: 'Category already exists' });
-    }
-
-    // Body of category
-    const category = new Category({
-      description,
-    });
-
-    await category.save();
-    console.log('Category created with success!\nCategory Id:', category._id);
-
-    res.status(201).json({ message: 'Category registered!' });
-  } catch (error) {
-    console.error('Category Registration Error:', error.message);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
-
 // Get category by id
 const getById = async (req, res) => {
   try {
     const { id } = req.params;
 
     const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({ message: 'Categoria não encontrada!' });
+    }
 
     res.status(200).json(category);
   } catch (error) {
@@ -55,12 +32,41 @@ const getById = async (req, res) => {
 const getByDescription = async (req, res) => {
   try {
     const { description } = req.params;
-    
-    const category = await Category.findOne({ description });;
+
+    const category = await Category.findOne({ description });
+    if (!category) {
+      return res.status(404).json({ message: 'Categoria não encontrada!' });
+    }
 
     res.status(200).json(category);
   } catch (error) {
     console.error('Get Category By Description Error:', error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Create a new category
+const create = async (req, res) => {
+  try {
+    const { description } = req.body;
+
+    // check if category already exists
+    const categoryExists = await Category.findOne({ description });
+    if (categoryExists) {
+      return res.status(409).json({ message: 'A categoria já existe!' });
+    }
+
+    // Body of category
+    const category = new Category({
+      description,
+    });
+
+    await category.save();
+    console.log('Category created with success!\nCategory Id:', category._id);
+
+    res.status(201).json({ message: 'Categoria adicionada com sucesso!' });
+  } catch (error) {
+    console.error('Category Registration Error:', error.message);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
@@ -74,7 +80,7 @@ const update = async (req, res) => {
     // Check if category exists
     const categoryExists = await Category.findOne({ description });
     if (categoryExists) {
-      return res.status(400).json({ message: 'Category already exists' });
+      return res.status(400).json({ message: 'A categoria já existe!' });
     }
 
     // Find category by id
@@ -84,7 +90,7 @@ const update = async (req, res) => {
     category.description = description;
 
     await category.save();
-    res.status(200).json({ message: 'Category updated' });
+    res.status(200).json({ message: 'Categoria atualizada com sucesso!' });
   } catch (error) {
     console.error('Update Category Error:', error.message);
     res.status(500).json({ message: 'Internal server error' });
@@ -95,8 +101,11 @@ const update = async (req, res) => {
 const remove = async (req, res) => {
   try {
     const { id } = req.params;
-    await Category.findByIdAndDelete(id);
-    res.status(200).json({ message: 'Category deleted' });
+    const cateogory = await Category.findByIdAndDelete(id);
+    if (!cateogory) {
+      return res.status(404).json({ message: 'Categoria não encontrada!' });
+    }
+    res.status(200).json({ message: 'Categoria removida com sucesso!' });
   } catch (error) {
     console.error('Delete Category Error:', error.message);
     res.status(500).json({ message: 'Internal server error' });
