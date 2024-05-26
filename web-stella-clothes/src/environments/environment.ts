4d2be83bf70d2cd15f89b5aa8eeea1b283f9446b
Sérgio Cruz <75474370+SergioMOCruz@users.noreply.github.com>
2024-05-26 14:34:35 +0100

    Update environment.ts

--- web-stella-clothes/src/environments/environment.ts
+++ web-stella-clothes/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3001',
+  apiUrl: 'https://loja-online-x9oa.onrender.com',
   stripePublishableKey: 'pk_test_51PGoGz2MF99qoT639XZAVycir64al7mqyGzWhQPQXwCzhCGeHE5OgD4T3Nw06o8SC1G9L1nPHUXmTo32s7hxJT4A00buTloKtp'
 };
